e4f7db46160bb07722ea34e7de0abc022a671ec5
Mohammad Talim <talimmohammad116@gmail.com>
2023-11-30 14:00:34 +0530

    Build the ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,89 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { SVGProps, useState } from 'react';
+import { JSX } from 'react/jsx-runtime';
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const [step, setStep] = useState(1);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex min-h-screen items-start bg-gradient-to-br from-slate-700 to-slate-900 pt-40">
+      <div className="mx-auto w-full max-w-md rounded-2xl bg-white">
+        <div className="flex justify-between rounded p-8">
+          <Step step={1} currentStep={step} />
+          <Step step={2} currentStep={step} />
+          <Step step={3} currentStep={step} />
+          <Step step={4} currentStep={step} />
+        </div>
+        <div className="px-8 pb-8">
+          <div>
+            <div className="mt-2 h-6 w-40 rounded bg-slate-100" />
+            <div className="mt-4 space-y-2">
+              <div className="h-4 w-5/6 rounded bg-slate-100" />
+              <div className="h-4 rounded bg-slate-100" />
+              <div className="h-4 w-4/6 rounded bg-slate-100" />
+            </div>
+          </div>
+
+          <div className="mt-10 flex justify-between">
+            <button
+              onClick={() => setStep(step < 2 ? step : step - 1)}
+              className="rounded px-2 py-1 text-slate-400 hover:text-slate-700"
+            >
+              Back
+            </button>
+            <button
+              onClick={() => setStep(step > 4 ? step : step + 1)}
+              className={`${
+                step > 4 ? 'pointer-events-none opacity-50' : ''
+              } bg flex items-center justify-center rounded-full bg-blue-500 py-1.5 px-3.5 font-medium tracking-tight text-white hover:bg-blue-600 active:bg-blue-700`}
+            >
+              Continue
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
-export default App;
+function Step({ step, currentStep }: { step: number; currentStep: number }) {
+  const status =
+    currentStep === step
+      ? 'active'
+      : currentStep < step
+        ? 'inactive'
+        : 'complete';
+
+  return (
+    <div
+      className={`${
+        status === 'active'
+          ? 'border-blue-500 bg-white text-blue-500'
+          : status === 'complete'
+            ? 'border-blue-500 bg-blue-500'
+            : 'border-slate-200 bg-white text-slate-400'
+      } flex h-10 w-10 items-center justify-center rounded-full border-2 font-semibold`}
+    >
+      <div className="flex items-center justify-center">
+        {status === 'complete' ? (
+          <CheckIcon className="h-6 w-6 text-white" />
+        ) : (
+          <span>{step}</span>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function CheckIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={3}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+    </svg>
+  );
+}
