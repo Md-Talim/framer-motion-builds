207537ee6c66e28b6c934ba2d964986dcca6f34b
Mohammad Talim <talimmohammad116@gmail.com>
2023-12-01 21:23:01 +0530

    Build the email page

---
+++ src/data.ts
@@ -0,0 +1,66 @@
+export const titles = [
+  [
+    "Master the Fundamentals of HTML & CSS",
+    "Discover the building blocks of the web and build beautiful websites.",
+  ],
+  [
+    "Unlock the Power of JavaScript",
+    "Learn how to make your webpages interactive and dynamic.",
+  ],
+  [
+    "Dive into the World of React",
+    "Build modern and scalable web applications with a popular framework.",
+  ],
+  [
+    "Conquer Responsive Design Principles",
+    "Craft websites that look stunning on all devices.",
+  ],
+  [
+    "Explore Modern CSS Frameworks & Libraries",
+    "Boost your development speed and efficiency with powerful tools.",
+  ],
+  [
+    "Master the Art of UI & UX Design",
+    "Design user-friendly interfaces that are both visually appealing and intuitive.",
+  ],
+  [
+    "Performance Optimization for Web Developers",
+    "Learn how to create blazing-fast websites that load in a snap.",
+  ],
+  [
+    "JavaScript Frameworks: A Comparative Analysis",
+    "Delve into the strengths and weaknesses of different popular frameworks.",
+  ],
+  [
+    "The Future of Front-End Development",
+    "Get insights into the latest trends and technologies shaping the future of web development.",
+  ],
+  [
+    "Debug Like a Pro: Essential Techniques & Tools",
+    "Learn how to diagnose and fix issues efficiently.",
+  ],
+  [
+    "Build Your Own Web Development Workflow",
+    "Optimize your workflow and maximize your productivity.",
+  ],
+  [
+    "Accessibility: Design for Everyone",
+    "Learn how to build inclusive websites that everyone can access and enjoy.",
+  ],
+  [
+    "Leverage the Power of Web APIs & Services",
+    "Integrate external data and functionalities into your web applications.",
+  ],
+  [
+    "SEO for Front-End Developers",
+    "Learn how to optimize your websites for search engines.",
+  ],
+  [
+    "From Front-End Developer to Full-Stack Engineer",
+    "Explore the path to becoming a full-fledged web developer.",
+  ],
+  [
+    "Building a Successful Career in Front-End Development",
+    "Learn the essential skills and strategies to thrive in the industry.",
+  ],
+];
